c3365eb94eb569d531e124c8641e6ccfc5cfb25a
supriyabansal <supriya323@gmail.com>
2022-03-14 12:39:12 +0100

    Updated Project structure

--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 import { IAlbum } from '../interfaces/Album';
 import { IPhoto } from '../interfaces/Photo';
 
@@ -10,14 +11,18 @@ export class AlbumService {
   constructor(private http: HttpClient) {}
 
   getAlbums() {
-    return this.http.get<IAlbum[]>(
-      `https://jsonplaceholder.typicode.com/users/1/albums`
+    return (
+      this.http.get<IAlbum[]>(
+        `https://jsonplaceholder.typicode.com/users/1/albums`
+      ) || of([])
     );
   }
 
   getAlbumPhotos(req: IAlbum) {
-    return this.http.get<IPhoto[]>(
-      `https://jsonplaceholder.typicode.com/albums/${req.id}/photos`
+    return (
+      this.http.get<IPhoto[]>(
+        `https://jsonplaceholder.typicode.com/albums/${req.id}/photos`
+      ) || of([])
     );
   }
 }
